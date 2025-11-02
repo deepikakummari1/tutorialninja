24ecf8d3e54d0999b93f7640cc84a275079e127a
deepikakummari1 <kmideepika123@gmail.com>
2025-11-02 11:51:16 +0530

    add3

--- utilities/sumvalue.py
+++ utilities/sumvalue.py
@@ -1,8 +1,16 @@
-a=12345
-while a>9:
-    s=0
-    for i in str(a):
-        s=s+int(i)
-    a=s
-print(a)
-print()
+# a=12345
+# while a>9:
+#     s=0
+#     for i in str(a):
+#         s=s+int(i)
+#     a=s
+# print(a)
+# print()
+
+l=[2,3,4,5,6]
+a=10
+for i in range(len(l)):
+    for o in range(i+1,len(l)):
+        sum=l[i]+l[o]
+        if sum==a:
+            print(l[i]+l[o])
