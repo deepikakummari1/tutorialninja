e8ae79883ea5a9909afc55a652615db19fac0054
deepikakummari1 <kmideepika123@gmail.com>
2025-11-06 12:43:54 +0530

    selemium

--- utilities/sumvalue.py
+++ utilities/sumvalue.py
@@ -1,11 +1,11 @@
-# a=12345
-# while a>9:
-#     s=0
-#     for i in str(a):
-#         s=s+int(i)
-#     a=s
-# print(a)
-# print()
+a=12345
+while a>9:
+    s=0
+    for i in str(a):
+        s=s+int(i)
+    a=s
+print(a)
+print()
 
 l=[2,3,4,5,6]
 a=10
